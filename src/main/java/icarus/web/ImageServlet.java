229d75b65e91b1bd70caf31091233a648f591a0b
Liviu Tudor <ltudor@netflix.com>
2016-03-29 00:17:56 -0700

    intermediary commit

--- src/main/java/icarus/web/ImageServlet.java
+++ src/main/java/icarus/web/ImageServlet.java
@@ -4,8 +4,10 @@ import javax.inject.Singleton;
 import javax.servlet.http.HttpServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Our image servlet.
@@ -19,5 +21,20 @@ public class ImageServlet extends HttpServlet {
     @Produces(MediaType.APPLICATION_JSON)
     public String getHello() {
         return "hello";
+    }
+
+    @GET
+    @Path("/png/{width}/{height}")
+    @Produces("image/png")
+    public Response getPng(@PathParam("width") int width, @PathParam("height") int height) {
+        try {
+
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    Response.ResponseBuilder dontCache(Response.ResponseBuilder builder) {
+        return builder.header("Expires", "-1").header("Cache-Control", "no-cache, no-store, must-revalidate").header("Pragma", "no-cache")
     }
 }
